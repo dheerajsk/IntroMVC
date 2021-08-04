8b58a24ce902799a346779a9717b53eb134e87ef
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2021-08-04 17:06:16 +0530

    Add project files.

---
+++ IntroMVC/Controllers/DataController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroMVC.Controllers
+{
+    public class DataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult ViewDataSubmit()
+        {
+            ViewData["name"] = Request.Form["name"];
+            ViewData["address"] = Request.Form["address"];
+            ViewData["class"] = Request.Form["class"];
+            ViewData["year"] = Request.Form["year"];
+
+            return View();
+        }
+    }
+}
---
+++ IntroMVC/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using IntroMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public ContentResult StudentAsString()
+        {
+            return Content("Name: Walter, Grade: 7");
+        }
+
+        public ViewResult StudentAsView()
+        {
+            ViewData["message"] = "Name: Walter, Grade: 7 (Coming with viewdata)";
+            return View();
+        }
+
+        public RedirectResult StudentAsRedirectResult()
+        {
+            return new RedirectResult("https://www.google.com/search?q=walter");
+        }
+
+        public RedirectToRouteResult StudentAsRedirectToRouteResult()
+        {
+            return new RedirectToRouteResult(new { Controller = "Home", Action = "StudentAsView" });
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
